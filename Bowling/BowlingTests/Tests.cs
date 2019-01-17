ef21a8e1e4b2fae6d8e1f0a52d89f8cf798a5003
marco <none@none.com>
2019-01-17 00:16:30 +0000

    update, plus script

---
+++ Bowling/BowlingTests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace BowlingTests
+{
+    
+    public class Tests
+    {
+        [Test]
+        public void DummyTest() {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
