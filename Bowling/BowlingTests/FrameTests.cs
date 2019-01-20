8b079c4b30a3600609e7a7099f2c1c5fdf2c3672
marco <none@none.com>
2019-01-20 10:47:39 +0000

    working

---
+++ Bowling/BowlingTests/FrameTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace BowlingTests
+{
+    public class FrameTests
+    {
+        [Test]
+        public void FrameMaximumScore() {
+            var maxValue = 30;
+            var actualValue = 15;
+
+            Assert.IsTrue(actualValue <= maxValue);
+        }
+    }
+}
