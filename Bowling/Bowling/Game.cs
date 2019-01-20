2959f9e3eb2efbf79259d8fd63b84bec5751f1dc
marco <none@none.com>
2019-01-20 10:32:18 +0000

    working

---
+++ Bowling/Bowling/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bowling
+{
+    public interface IGame {
+        int CalculateScore();
+    }
+    public class Game : IGame
+    {
+        public int CalculateScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bowling/save.ps1
@@ -0,0 +1,25 @@
+$msbuild ='C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe'
+$sol = 'C:\development\projects\bowling\Bowling\Bowling.sln '
+$opt1 = '/t:rebuild'
+$opt2 = '/verbosity:quiet'
+
+& $msbuild $sol $opt1 $opt2
+
+if ($LASTEXITCODE -eq 0) {
+    $nunit_console = 'C:\development\projects\bowling\Bowling\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe'
+    $tests = 'C:\development\projects\bowling\Bowling\BowlingTests\bin\Debug\BowlingTests.dll'
+    $opt1 = '--stoponerror'
+    $opt2 = '--noresult'
+
+    & $nunit_console $tests $opt1 $opt2
+}
+
+$testsPassed = $LASTEXITCODE -eq 0
+
+if ($testsPassed) {
+	git add --all
+    git commit -am working
+} 
+else {
+    git reset --hard
+}
