626714fb082d72e2ba8375a06b0eccfe97ea0723
marco <none@none.com>
2019-01-20 22:50:01 +0000

    working

--- Bowling/BowlingTests/FrameTests.cs
+++ Bowling/BowlingTests/FrameTests.cs
@@ -7,19 +7,6 @@ namespace BowlingTests
     public class FrameTests
     {
         [Test]
-        public void ScoreIsTheSumOfTwoRollsIfTheirSumIsLesserThenTen()
-        {
-            var firstRoll = new Roll(4);
-            var secondRoll = new Roll(2);
-            var expectedResult = 6;
-
-            var frame = Frame.CreateFrameFromRolls(firstRoll, secondRoll);
-            var calculatedValue = frame.CalculateScore();
-
-            Assert.AreEqual(expectedResult, calculatedValue);
-        }
-
-        [Test]
         public void AllPinsDownInTwoRollsRequireBonusPoints() {
             var firstRoll = new Roll(4);
             var secondRoll = new Roll(6);
@@ -60,6 +47,29 @@ namespace BowlingTests
             .TypeOf<InvalidOperationException>()
             .With.Message
             .EqualTo("This frame require a bonus to be applied"));
+
+            firstRoll = new Roll(10);
+            secondRoll = new Roll(0);
+            frame = Frame.CreateFrameFromRolls(firstRoll, secondRoll);
+
+            Assert.That(() => frame.CalculateScore(),
+            Throws.Exception
+            .TypeOf<InvalidOperationException>()
+            .With.Message
+            .EqualTo("This frame require a bonus to be applied"));
+        }
+
+        [Test]
+        public void ScoreIsTheSumOfTwoRollsIfTheirSumIsLesserThenTen()
+        {
+            var firstRoll = new Roll(4);
+            var secondRoll = new Roll(2);
+            var expectedResult = 6;
+
+            var frame = Frame.CreateFrameFromRolls(firstRoll, secondRoll);
+            var calculatedValue = frame.CalculateScore();
+
+            Assert.AreEqual(expectedResult, calculatedValue);
         }
     }
 }
