e0ee570985c5487607917b1d6d8ca342934c655c
marco <none@none.com>
2019-01-21 22:20:40 +0000

    working

---
+++ Bowling/Bowling/Model/RegularFrame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Bowling.Model
+{
+    public class RegularFrame : Frame
+    {
+        public RegularFrame(List<Roll> rolls) : base (rolls){}
+
+        public override int CalculateScore()
+        {
+            var score = 0;
+
+            foreach (Roll roll in _baseRolls)
+            {
+                score += roll.GetScore();
+            }
+            return score;
+        }
+
+        public override bool IsBonusRequired() => false;
+    }
+}
