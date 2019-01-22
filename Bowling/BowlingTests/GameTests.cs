c4cdd7560812a2f37d8fe8f406ae7e0c9543ceec
marco <none@none.com>
2019-01-22 23:12:24 +0000

    working

--- Bowling/BowlingTests/GameTests.cs
+++ Bowling/BowlingTests/GameTests.cs
@@ -32,49 +32,6 @@ namespace BowlingTests
                     yield return new TestCaseData(new List<string>() { "3", "7", "3", "5" }).Returns((10 + 3) + (3 + 5));
                 }
             }
-
-            public static List<string> GetTestSequence()
-            {
-                return new List<string>()
-                {
-                    //1
-                    "1",
-                    "1",
-                    //2
-                    "1",
-                    "9",
-                    //3
-                    "1",
-                    "1",
-                    //4
-                    "10",
-                    "X",
-                    //5
-                    "1",
-                    "1",
-                    //6
-                    "1",
-                    "1",
-                    //7
-                    "1",
-                    "1",
-                    //8
-                    "1",
-                    "1",
-                    //9
-                    "1",
-                    "1",
-                    //10
-                    "1",
-                    "1",
-                    //11
-                    "1",
-                    "1",
-                    //12
-                    "1",
-                    "1",
-                };
-            }                       
         }
     }
 }
